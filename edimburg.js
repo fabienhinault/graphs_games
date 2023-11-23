4b492807c76cb8cb3e232839a68a5b9d104ec66d
Fabien Hinault <fabien.hinault@free.fr>
2023-11-23 20:47:01 +0100

    remove debug log

--- edimburg.js
+++ edimburg.js
@@ -4,12 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let current;
     document.body.onclick = (event) => {
         const tmp = event.target.closest('svg > g > g.node');
-        console.debug(tmp);
         if (tmp) {
             last = current;
             current = tmp;
             let ellipse = current.querySelector('ellipse')
-            console.log(ellipse);
             ellipse.setAttribute('fill', 'gray');
             if (last !== undefined) {
                 document.querySelectorAll(`g._${last.id}`).forEach(_ => _.remove());
