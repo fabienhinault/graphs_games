32f1ab4394092c50008e0ac5b74bae57b34d07aa
Fabien Hinault <fabien.hinault@free.fr>
2023-12-01 15:30:27 +0100

    enlarge vertices

--- edinburgh.js
+++ edinburgh.js
@@ -1,5 +1,18 @@
 "use strict";
+function enlargeVertices() {
+    document.querySelectorAll('g.node').forEach(g => {
+        // all vertices seem to be at least 18 away from each other.
+        const extraRadius = 9;
+        const ellipse = g.querySelector('ellipse');
+        const clonedEllipse = ellipse.cloneNode(false);
+        clonedEllipse.removeAttribute('stroke');
+        clonedEllipse.setAttribute('rx', Number(clonedEllipse.getAttribute('rx')) + extraRadius);
+        clonedEllipse.setAttribute('ry', Number(clonedEllipse.getAttribute('ry')) + extraRadius);
+        ellipse.insertAdjacentElement('beforebegin', clonedEllipse);
+    });
+}
 document.addEventListener('DOMContentLoaded', function() {
+    enlargeVertices();
     let last;
     let current;
     let possibleIds;
@@ -8,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (tmp && (possibleIds === undefined || possibleIds.includes(tmp.id))) {
             last = current;
             current = tmp;
-            let ellipse = current.querySelector('ellipse')
+            let ellipse = current.querySelector('ellipse + ellipse')
             ellipse.setAttribute('fill', 'gray');
             if (last !== undefined) {
                 document.querySelectorAll(`g._${last.id}`).forEach(_ => _.remove());
