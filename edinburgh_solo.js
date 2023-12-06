8198b17004b5e1bdb196484302b56beaf29f4be0
fabienhinault <fabienhinault@users.noreply.github.com>
2023-12-06 18:34:05 +0100

    Update edinburgh_solo.js

--- edinburgh_solo.js
+++ edinburgh_solo.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//deep clone
+let initialNextss = JSON.parse(JSON.stringify(nextss));
 let last;
 let current;
 let currentId;
@@ -7,7 +9,21 @@ let possibleIds;
 let winnings = new Set();
 let losings = new Set();
 const game = [];
+const firstPlayer = 1000;
+const secondPlayer = 0;
+const probableFirstPlayer = 750;
+const probableSecondPlayer = 250;
+const unsure = 500;
+const sequenceValuesMap = new Map([
+    [probableSecondPlayer, probableSecondPlayer],
+    [(probableSecondPlayer + unsure) / 2 , probableSecondPlayer],
+    [unsure, unsure],
+    [(unsure + probableFirstPlayer) / 2, probableFirstPlayer], 
+    [probableFirstPlayer, probableFirstPlayer]]);
 
+function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt);
+}
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -53,12 +69,71 @@ function argsMin(array, f) {
     return min(array, f).elements;
 }
 
-function evaluatePlay(nextId) {
-    const storedValue = localStorage.getItem([...game, nextId]);
-    if (storedValue !== null) {
-        return storedValue;
+function getLastPlayer(game) {
+    return game.length % 2 * firstPlayer;
+}
+
+function otherPlayer(player) {
+    return firstPlayer - player;
+}
+
+function probablePlayer(player) {
+    return (unsure + player) / 2;
+}
+
+function sum(array) {
+    return array.reduce((acc, cur) => acc + cur);
+}
+
+function average(array) {
+    return sum(array) / array.length;
+}
+
+function evaluateSequence(sequence) {
+    const lastMove = sequence[sequence.length - 1];
+    // player who just played last
+    const lastPlayer = getLastPlayer(sequence);
+    const nextPlayer = otherPlayer(lastPlayer);
+    const nexts = initialNextss[lastMove].filter(_ => !sequence.includes(_));
+    const nextsValues = new Set(nexts.map(next => getSequenceValue([...sequence, next])));
+    if (nextsValues.has(nextPlayer)) {
+        return nextPlayer;
     }
-    return 0.5;
+    if (nextsValues.has(lastPlayer)) {
+        if (nextsValues.size === 1) {
+            return lastPlayer;
+        } else {
+            return probablePlayer(lastPlayer);
+        }
+    }
+    const entry = average([...nextsValues])
+    if (!sequenceValuesMap.has(entry)) {
+        throw new Error('absent nextsValues', entry);
+    }
+    return sequenceValuesMap.get(entry);
+}
+
+function evaluateSubsequences(sequence) {
+    range(sequence.length - 1, 1).reverse().map(_ => sequence.slice(0, _)).forEach((subsequence) => {
+        const value = evaluateSequence(subsequence);
+        if (value !== unsure) {
+            localStorage.setItem(subsequence, value);
+        } else {
+            localStorage.removeItem(subsequence);
+        }
+    });
+}
+
+function getSequenceValue(sequence) {
+    const storedValue = localStorage.getItem(sequence);
+    if (storedValue !== null) {
+        return Number(storedValue);
+    }
+    return unsure;
+}
+
+function evaluatePlay(nextId) {
+    return getSequenceValue([...game, nextId]);
 }
 
 function enlargeVertices() {
@@ -89,15 +164,15 @@ document.addEventListener('DOMContentLoaded', function() {
         const gam = game.slice(0, game.length -1);
         const winning = possibleIds.find(id => winnings.has(id));
         if (winning) {
-            localStorage.setItem(game, 0);
+            localStorage.setItem(game, secondPlayer);
             return winning;
         }
         const notLosings = possibleIds.filter(id => !losings.has(id));
         if (notLosings.length >= 1) {
             return pick(argsMin(notLosings, evaluatePlay));
         }
-        localStorage.setItem(game, 1);
-        localStorage.setItem(gam, 1);
+        localStorage.setItem(game, firstPlayer);
+        localStorage.setItem(gam, firstPlayer);
         return pick(possibleIds);
     }
 
@@ -123,8 +198,9 @@ document.addEventListener('DOMContentLoaded', function() {
         if (possibleIds.length === 0) {
             // the values are: 1 if the first player wins, 0 if the second player wins.
             // As the bot always plays second, 0 are winning games, 1 are losing games.
-            localStorage.setItem(game, game.length % 2);
-            localStorage.setItem(game.slice(0, game.length -1), game.length % 2);
+            localStorage.setItem(game, getLastPlayer(game));
+            localStorage.setItem(game.slice(0, game.length -1), getLastPlayer(game));
+            evaluateSubsequences(game);
         }
     }
 
