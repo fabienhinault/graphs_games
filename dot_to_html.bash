08e03e9576c851586df44903c27ed6db24d645b1
Fabien Hinault <fabien.hinault@free.fr>
2024-01-05 19:07:40 +0100

    trophy

--- dot_to_html.bash
+++ dot_to_html.bash
@@ -12,7 +12,7 @@ dot -Tsvg $DOTFILE -o ${BASENAME}.svg
 sed "/<!-- insert svg content just above this line -->/e cat ${BASENAME}.svg" ./edinburgh.html | \
 sed "/\/\/ insert json content just above this line/e  cat ${BASENAME}.json ; echo" | \
 sed "/<!-- insert script markup for edinburgh.js just above this line -->/i <script src=\"$JSPATH\"></script>" | \
-sed -r "s#src=\"([a-z_]*)\\.svg\"#src=\"$ROOTPATH/\\1\\.svg\"#" | \
+sed -r "s#src=\"([a-zA-Z0-9_]*)\\.svg\"#src=\"$ROOTPATH/\\1\\.svg\"#" | \
 sed "s#href=\"edinburgh.css\"#href=\"$ROOTPATH/edinburgh.css\"#" > \
 ${BASENAME}.html
 SOLO=$(echo $BASENAME | sed "s#$DOTDIR\/#$DOTDIR\/solo_#")
--- edinburgh.js
+++ edinburgh.js
@@ -11,7 +11,110 @@ function enlargeVertices() {
         ellipse.insertAdjacentElement('beforebegin', clonedEllipse);
     });
 }
+
+class Clock {
+    getTime() {
+        return Date.now();
+    }
+}
+
+class Game {
+    constructor(nextss, initialNextss, sequenceValueStorage, clock, gameOverCallback) {
+        this.nextss = nextss;
+        this.moves = [];
+        this.possibleNexts = undefined;
+        this.initialNextss = initialNextss;
+        this.sequenceValueStorage= sequenceValueStorage;
+        this.clock = clock;
+        this.gameOverCallback = gameOverCallback;
+    }
+    
+    copy() {
+        const result = new Game([...this.nextss.map(_ => [..._])], this.initialNextss, this.sequenceValueStorage, this.clock, null);
+        result.moves = [...this.moves];
+        return result;
+    }
+
+    getCurrentMove() {
+        return this.moves[this.moves.length - 1];
+    }
+
+    getPreviousMove() {
+        return this.moves[this.moves.length - 2];
+    }
+
+    getCurrentPlayer() {
+        return (this.moves.length % 2) * firstPlayer;
+    }
+
+    minmax(nextPlayer, nextsValues) {
+        if (nextPlayer === firstPlayer) {
+            return Math.max(...nextsValues);
+        } else {
+            return Math.min(...nextsValues);
+        }
+    }
+
+    getSequenceValue(sequence) {
+        const storedValue = this.sequenceValueStorage.getValue(sequence);
+        if (storedValue !== null && storedValue !== undefined) {
+            return Number(storedValue);
+        }
+        return unsure;
+    }
+
+    evaluateNexts(time) {
+        if (this.clock.getTime() < time) {
+            for (let next of this.possibleNexts) {
+                const game = this.copy();
+                game.play(next);
+                setTimeout(() => game.evaluateNexts(time), 0);
+            }
+        }
+    }
+
+    play(current) {
+        this.moves.push(current);
+        this.possibleNexts = [...this.nextss[current]];
+        this.nextss = this.nextss.map((nexts, iNexts) => {
+            return nexts.filter(next => next != current);
+        });
+        this.nextss[current] = [];
+        if (this.possibleNexts.length === 0) {
+            // the game is over
+            this.gameOverCallback?.();
+        }
+    }
+
+    getMoveValue(nextId) {
+        return this.getSequenceValue([...this.moves, nextId]);
+    }
+}
+
+class LocalStorageSequenceValueStorage {
+    storeValue(sequence, value) {
+        localStorage.setItem(sequence, value);
+    }
+    getValue(sequence) {
+        return localStorage.getItem(sequence);
+    }
+    removeValue(sequence) {
+        localStorage.removeItem(sequence);
+    }
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    function onGameOver() {
+        const klass = ['player2_won', 'player1_won'][game.moves.length % 2];
+        ['#player1_won', '#player2_won']
+            .map(idSelector => document.querySelector(idSelector))
+            .forEach(img => img.setAttribute('class', klass));
+    }
+
+    const game = new Game(nextss, JSON.parse(JSON.stringify(nextss)), new LocalStorageSequenceValueStorage(), new Clock(), onGameOver);
     enlargeVertices();
     let prepreviousId;
     let previous;
@@ -19,6 +122,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let possibleIds;
 
     function getNodeId(node) {
+        // pass prefix "id"
         return node.id.substring(2);
     }
 
@@ -28,6 +132,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         previous = current;
         current = tmp;
+        game.play(getNodeId(current));
         updateSvgCurrentVertex();
         if (previous !== undefined) {
             updateSvgpreviousVertex(previous);
